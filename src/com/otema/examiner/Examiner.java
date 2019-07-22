f243592540b47f4a75e23ca5fd2ea8c960ac849f
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-22 13:37:54 +0300

    MADE THE CONNECTION OBJECT DYNAMIC

--- src/com/otema/examiner/Examiner.java
+++ src/com/otema/examiner/Examiner.java
@@ -21,7 +21,7 @@ public class Examiner extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {    
         launch(args);
     }
     
