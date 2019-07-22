54e4aa1ee01c818c3af13856495d769fec8e59d7
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-22 13:11:11 +0300

    Added db connection code

--- src/com/otema/examiner/resources/db/connection.java
+++ src/com/otema/examiner/resources/db/connection.java
@@ -1,9 +1,26 @@
 package com.otema.examiner.resources.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author TheOnlySmartBoy
  */
 public class connection {
-    
+
+    public void connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo", "root", "root");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(connection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(connection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
