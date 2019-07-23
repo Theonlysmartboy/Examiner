01d9d1076d6b1e094f2f2ec2020dcf331f7a6146
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-23 14:32:45 +0300

    Begun writing the code to handle the transition and progress

--- src/com/otema/examiner/FXMLDocumentController.java
+++ src/com/otema/examiner/FXMLDocumentController.java
@@ -13,25 +13,32 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author TheOnlySmartBoy
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     private Label label;
     @FXML
     private AnchorPane root;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void closeApp(MouseEvent event) {
-        
+        Object[] options = {"Yes", "No"};
+        int n = JOptionPane.showOptionDialog(null, "Do you want to Exit the System? ", "Examiner", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (n == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        } else {
+
+        }
     }
-    
+
 }
