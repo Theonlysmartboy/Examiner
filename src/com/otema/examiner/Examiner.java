2a3d4930447cb9056fa760c3d680faccac08305c
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-23 14:23:14 +0300

    Loaded the Interface scene

--- src/com/otema/examiner/Examiner.java
+++ src/com/otema/examiner/Examiner.java
@@ -5,24 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author TheOnlySmartBoy
  */
 public class Examiner extends Application {
-        @Override
+
+    private static Stage stage = null;
+
+    @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
+        this.stage = stage;
     }
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {    
+    public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
