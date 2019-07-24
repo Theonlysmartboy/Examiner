e9fe53ed57638ace64389707b2a6e7f5566f03b8
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-24 17:24:51 +0300

    Completed writing the interface code for password reset Wrote the code to handle the close event

--- src/com/otema/examiner/auth/PasswordResetController.java
+++ src/com/otema/examiner/auth/PasswordResetController.java
@@ -1,10 +1,18 @@
 package com.otema.examiner.auth;
 
+import com.otema.examiner.FXMLDocumentController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -23,9 +31,30 @@ public class PasswordResetController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-static boolean isValid(String email) {
-      String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
-      return email.matches(regex);
-   }    
-    
+
+    static boolean isValid(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+
+    @FXML
+    private void closeApp(MouseEvent event) {
+        //Check for and handle accidental opening and clossing for app
+        Object[] options = {"Yes", "No"};
+        int n = JOptionPane.showOptionDialog(null, "Do you want to Exit the System? ", "Reset Password", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (n == JOptionPane.YES_OPTION) {
+            System.exit(0);
+        } else {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+                rootPane.getChildren().removeAll();
+                rootPane.setMinSize(700.0, 400.0);
+                rootPane.getChildren().setAll(root);
+
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
 }
