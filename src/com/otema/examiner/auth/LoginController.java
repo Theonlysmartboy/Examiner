3b2dd891f4039c14b0cd5a460d16ba20ef64c8a6
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-24 10:58:09 +0300

    Added login files

---
+++ src/com/otema/examiner/auth/LoginController.java
@@ -0,0 +1,37 @@
+package com.otema.examiner.auth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author TheOnlySmartBoy
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private AnchorPane root;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void loadRegistration(MouseEvent event) {
+    }
+
+    @FXML
+    private void closeApp(MouseEvent event) {
+        
+    }
+    
+}
